02d333c40ac1d967badaf2bb784edcb41a77fc44
Huynh Ngoc Qui <quichua333@gmail.com>
2023-04-14 22:01:03 +0700

    fix + addview

---
+++ HotelManagement/View/Staff/RoomCatalogManagement/RoomInfo/RoomFunitureInfo.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HotelManagement.View.Staff.RoomCatalogManagement.RoomInfo
+{
+    /// <summary>
+    /// Interaction logic for RoomFunitureInfo.xaml
+    /// </summary>
+    public partial class RoomFunitureInfo : Window
+    {
+        public RoomFunitureInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();    
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        
+    }
+}
