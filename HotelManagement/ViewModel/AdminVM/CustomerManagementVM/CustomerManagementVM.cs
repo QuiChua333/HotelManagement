c132093e90f65e3342f63d685da613ca97b38891
Fatitboo <21522448@gm.uit.edu.vn>
2023-04-18 22:51:57 +0700

    ..

---
+++ HotelManagement/ViewModel/AdminVM/CustomerManagementVM/CustomerManagementVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HotelManagement.ViewModel.AdminVM.CustomerManagementVM
+{
+    public class CustomerManagementVM : BaseVM
+    {
+        public ICommand FirstLoadCM { get; set; }
+        public CustomerManagementVM()
+        {
+            FirstLoadCM = new RelayCommand<System.Windows.Controls.Page>((p) => { return true; }, (p) =>
+            {
+            });
+
+        }
+    }
+}
