39a063b2e8e280aa4476dd542cb46a64a6d5b8f7
21522683 <21522683@gm.uit.edu.vn>
2023-04-22 16:27:55 +0700

    Tinh fix BookingRoom

--- HotelManagement/View/BookingRoomManagement/DetailRent.xaml.cs
+++ HotelManagement/View/BookingRoomManagement/DetailRent.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace HotelManagement.View.Admin.BookingRoomManagement
+namespace HotelManagement.View.BookingRoomManagement
 {
     /// <summary>
     /// Interaction logic for DetailRent.xaml
