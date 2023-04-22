3516703f1fc0e67bf1040c5d07f7acc02c80034e
Huynh Ngoc Qui <quichua333@gmail.com>
2023-04-22 16:13:41 +0700

    add nav

--- HotelManagement/ViewModel/AdminVM/AdminVM.cs
+++ HotelManagement/ViewModel/AdminVM/AdminVM.cs
@@ -1,4 +1,5 @@
 ﻿using CinemaManagementProject.Utilities;
+using HotelManagement.ViewModel.StaffVM.RoomCatalogManagementVM;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+
 
 namespace HotelManagement.ViewModel.AdminVM
 {
@@ -20,16 +22,18 @@ namespace HotelManagement.ViewModel.AdminVM
         public ICommand FirstLoadCM { get; set; }
         private void Furniture(object obj) => CurrentView = new FurnitureManagementVM.FurnitureManagementVM();
         private void Service(object obj) => CurrentView = new ServiceManagementVM.ServiceManagementVM();
+        private void RoomCatalog(object obj) => CurrentView = new RoomCatalogManagementVM();
 
         public ICommand FurnitureCommand { get; set; }
         public ICommand ServiceCommand { get; set; }
+        public ICommand RoomCatalogCommand { get; set; }
         public AdminVM()
         {
             _currentView = new ServiceManagementVM.ServiceManagementVM();
 
             FurnitureCommand = new RelayCommand(Furniture);
             ServiceCommand = new RelayCommand(Service);
-
+            RoomCatalogCommand = new RelayCommand(RoomCatalog);
         }
     }
 }
