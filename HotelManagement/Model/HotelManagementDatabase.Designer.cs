31102841bf280988273c29b2ae66306b6dcfaa99
21522402 <21522402@gm.uit.edu.vn>
2023-05-27 09:47:10 +0700

    Merge Tinh2

--- HotelManagement/Model/HotelManagementDatabase.Designer.cs
+++ HotelManagement/Model/HotelManagementDatabase.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\CNPM_PTTKHTTT\HotelManagement\HotelManagement\Model\HotelManagementDatabase.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\NhanLe\Desktop\PTTKHTTT\HotelManagement\HotelManagement\Model\HotelManagementDatabase.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
