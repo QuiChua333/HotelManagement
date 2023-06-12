1be80eb6b5d39f054abddf16084d7a44dd604a29
21522402 <21522402@gm.uit.edu.vn>
2023-06-12 22:24:40 +0700

    Nnhna

--- HotelManagement/ViewModel/AdminVM/TroubleManagementVM/TroubleManagementVM.cs
+++ HotelManagement/ViewModel/AdminVM/TroubleManagementVM/TroubleManagementVM.cs
@@ -242,7 +242,8 @@ namespace HotelManagement.ViewModel.AdminVM.TroubleManagementVM
                     wd.gridbyHotel.Visibility = Visibility.Collapsed;
                     wd.gridByCus.Visibility = Visibility.Visible;
                     wd.predictgrid.Visibility = Visibility.Visible;
-                    PredictPrice = (double)(await TroubleService.Ins.GetTroubleByCus(SelectedItem.TroubleId)).PredictedPrice;
+                    if((await TroubleService.Ins.GetTroubleByCus(SelectedItem.TroubleId)) != null)
+                        PredictPrice = (double)(await TroubleService.Ins.GetTroubleByCus(SelectedItem.TroubleId)).PredictedPrice;
                     wd.MSPgrid.Visibility=System.Windows.Visibility.Visible;
                     wd.rentalcontractid.Text = (await TroubleService.Ins.GetTroubleByCus(SelectedItem.TroubleId)).RentalContractId;
                 }
